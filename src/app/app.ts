30ea7241d7b86da8c3ad666500cbf2705a3510d5
Borges <borgespaulo72@yahoo.com.br>
2026-06-17 14:33:46 -0300

    feat: Incluindo nav e footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserList } from './user-list/user-list';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, UserList],
+  imports: [RouterOutlet, UserList, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
